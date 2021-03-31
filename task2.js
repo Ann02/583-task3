22be62b40b46e1ff177c8e2e0fd747111860c476
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-01 00:27:47 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,7 @@
+for(let count = 0; count <= 15; count++) {
+  if ( count >= 0 && (count % 2) == 0 ){
+    console.log(count + ' є парним додатним');
+  }else{
+    console.log(count + ' є непарним додатним');
+  }
+}
