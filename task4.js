36ae9b0abf01d65e6ae27c5110be1918974f4397
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-01 02:52:34 +0300

    Create task4.js

---
+++ task4.js
@@ -0,0 +1,14 @@
+function multiplication(x, y) {
+  let result = x ** y;
+  return result;
+} 
+let x = prompt('Enter the length of the first side ');  x = Number(x);
+let y = prompt('Enter the length of the first side ');  y = Number(y);
+console.log(result);
+
+function multiplication(a, b) {
+   let result = a ** b;
+   console.log(result);
+}
+
+multiplication(2, 3); 
