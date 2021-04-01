f05ffa136e47e9cc5c55a34dd587c4785a1d50be
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-01 13:33:07 +0300

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,14 +1,7 @@
 function multiplication(x, y) {
   let result = x ** y;
-  return result;
+  console.log(result)
 } 
-let x = prompt('Enter the length of the first side ');  x = Number(x);
-let y = prompt('Enter the length of the first side ');  y = Number(y);
-console.log(result);
-
-function multiplication(a, b) {
-   let result = a ** b;
-   console.log(result);
-}
-
-multiplication(2, 3); 
+let x = prompt('Введіть число ');  x = Number(x);
+let y = prompt('Введіть степінь ');  y = Number(y);
+multiplication(x, y);
