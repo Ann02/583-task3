7b3e414fed7e4fb01c5292b0ad0ce593665bcd5a
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-01 11:29:21 +0300

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,13 @@
+function findUnique(arr)
+{   
+    var n = arr.length;
+    for (var i = 0; i < n-1; i++){
+      for (var j = i+1; j < n; j++){
+        if (arr[ i ] === arr[j]){
+          return false;
+        }
+        }
+     }
+    return true;
+}
+//findUnique([1, 2, 3, 3, 5])
