1e35d9bf5b895461d93085eb27bc09424cd41e44
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-03 21:23:47 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -10,6 +10,6 @@ function findUnique(arr)
      }
     return true;
 }
-//findUnique([1, 2, 3, 5, 3]);  // => false
-//findUnique([1, 2, 3, 5, 11]); // => true
+//findUnique([1, 2, 3, 5, 3]);  //
+//findUnique([1, 2, 3, 5, 11]); //
 
