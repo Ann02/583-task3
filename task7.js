4d9b7c8e86cf64ed6305143325149342f22c30cc
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-06 14:56:02 +0300

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,4 +1,4 @@
-var arr = [];
+/*var arr = [];
     for(var i=0; i < 4; i++) {
       arr[i] = prompt('Введіть '); 
     }
@@ -6,4 +6,14 @@ function lastElem(){
   var y = arr.slice (- 1);
   console.log(y);
 }
-lastElem()
+lastElem()*/
+function lastElem(arr, count){
+    if (count == null){
+        console.log(arr.pop());
+    }else{
+        console.log(arr.slice(-count));
+    }
+}
+console.log(lastElem([3, 4, 10, -5]));
+console.log(lastElem([3, 4, 10, -5], 2));
+
