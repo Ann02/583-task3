701739446fe4dfc8b234fe797ef4e0292b8e45ea
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-05-04 23:40:49 +0300

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,4 +1,17 @@
-function ucFirst(str) {
-  return str[0].toUpperCase() + str.slice(1);
+let str = "i love java script";
+
+function changeReg(string) {
+  const strArray = string.split(" ");
+  const arr = [];
+  for (let index = 0; index < strArray.length; index++) {
+    arr[index] =
+      strArray[index].charAt(0).toUpperCase() + strArray[index].slice(1);
+  }
+  let resStr = "";
+  for (let index = 0; index < arr.length; index++) {
+    resStr += arr[index] + " ";
+  }
+  return resStr;
 }
-//ucFirst("hello")
+
+console.log(changeReg(str));
